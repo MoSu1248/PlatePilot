1f415e8ba67bf757dbbcb59e296432a33bf672ce
MoSu1248 <mohammedsuhailr2@gmail.com>
2026-03-10 12:50:29 +0200

    feats: added navbar with styling and routing to application

---
+++ src/app/core/navbar/navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Logo } from '../../shared/icons/logo/logo';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive, Logo],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import { Home } from './app/features/home/home';
+import { Recipe } from './app/features/recipe/recipe';
+import { Favourites } from './app/features/favourites/favourites';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter([
+      { path: '', component: Home },
+      { path: 'recipes', component: Recipe },
+      { path: 'favorites', component: Favourites },
+    ]),
+  ],
+});
